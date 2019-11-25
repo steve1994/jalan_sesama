7a3bd80ea27dfabb8e89d8922184a9c375de39e9
steve1994 <steve.harnadi@gmail.com>
2019-11-25 16:01:24 +0700

    First Commit Final Project for Client Android

---
+++ client/src/components/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Container,
+        Header,
+        Title,
+        Content,
+        Footer,
+        FooterTab,
+        Button,
+        Left,
+        Right,
+        Body,
+        Icon,
+        Text } from 'native-base';
+
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <Container>
+            <Header>
+              <Left>
+                <Button transparent>
+                  <Icon name='menu' />
+                </Button>
+              </Left>
+              <Body>
+                <Title>Header</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content>
+              <Text>
+                This is Content Section
+              </Text>
+            </Content>
+            <Footer>
+              <FooterTab>
+                <Button full>
+                  <Text>Footer</Text>
+                </Button>
+              </FooterTab>
+            </Footer>
+          </Container>
+        );
+    }
+
+}
