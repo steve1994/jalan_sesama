36d3242426a56b6a585833ce5b583a7cfa744d4a
steve1994 <steve.harnadi@gmail.com>
2019-11-27 18:57:52 +0700

    Add New Field Into Users

--- server/model/users.js
+++ server/model/users.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     nama: String,
-    alamat: String
+    alamat: String,
+    username: String,
+    password: String,
+    foto: String
 })
 
 module.exports = mongoose.model('userSchema',userSchema);
--- server/routes/users.js
+++ server/routes/users.js
@@ -28,13 +28,48 @@ router.get('/:idUser', function (req,res) {
 router.post('/', function (req,res) {
     let nama = req.body.nama;
     let alamat = req.body.alamat;
+    let username = req.body.username;
+    let password = req.body.password;
     try {
-        const newUsers = new Users({nama,alamat});
+        const newUsers = new Users({nama,alamat,username,password});
         newUsers.save().then(dataCreated => {
             res.status(201).json({status:'success',data:dataCreated})
         })
     } catch (error) {
         res.status(400).json({status:'failed',error});
+    }
+})
+
+function randomString(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+router.put('/uploadphoto/:idUser', function (req,res) {
+    let idUser = req.params.idUser;
+    let uploadedFile = req.files ? req.files.files : null;
+    let fileName = req.files ? (randomString(10) + "_" + req.files.files.name) : null;
+    if (uploadedFile) {
+        uploadedFile.mv(path.join(__dirname,`../public/images/uploaded_image/user/${fileName}`), function (err) {
+            if (err) {
+                res.status(400).json({status:'failed',error:err})
+            } else {
+                Users.findOneAndUpdate({_id:idUser},{foto:fileName},function (err,response) {
+                    if (err) {
+                        res.status(400).json({status:'failed',error:err});
+                    } else {
+                        res.status(201).json({status:'success',data:response});
+                    }
+                })
+            }
+        })
+    } else {
+        res.status(400).json({status:'failed',error:'file uploaded is empty'});
     }
 })
 
