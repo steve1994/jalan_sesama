b369dc302bfcc5292d25ea29c11d5b8c9be9554c
steve1994 <steve.harnadi@gmail.com>
2019-12-15 12:51:11 +0700

    Login still in problem

--- admin/src/reducers/user.js
+++ admin/src/reducers/user.js
@@ -2,7 +2,6 @@ const user = (state = {}, action) => {
     switch(action.type) {
         case 'POST_REGISTER_USER_SUCCESS':
         case 'POST_LOGIN_USER_SUCCESS':
-            console.log("MASUK JUGA KE REDUCER");
             return action.dataUser;
         case 'POST_REGISTER_USER_FAILURE':
         case 'POST_LOGIN_USER_FAILURE':
